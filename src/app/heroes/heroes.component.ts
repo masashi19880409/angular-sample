5b6ebb99611aabd48a7d639d623fb5a28d63f76c
masashi19880409 <mamemasa46.90@gmail.com>
2020-03-30 00:44:43 +0900

    angular sample

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  public hero: Hero = {
+    id: 1,
+    name: 'Yamada'
+  };
+
+  selected: boolean = false;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  clieckName(): void {
+    if (this.selected) {
+      this.selected = false;
+    } else {
+      this.selected = true;
+    }
+  }
+}
